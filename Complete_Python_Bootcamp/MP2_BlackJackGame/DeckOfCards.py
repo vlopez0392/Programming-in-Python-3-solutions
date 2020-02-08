b8a50a74a49b50fef07fa8378133a02643c2c53a
Vick <vlopez0392@gmail.com>
2020-02-09 00:05:35 +0800

    Milestone Project 2 Blackjack game Progress II DeckOfCards

--- Complete_Python_Bootcamp/MP2_BlackJackGame/DeckOfCards.py
+++ Complete_Python_Bootcamp/MP2_BlackJackGame/DeckOfCards.py
@@ -1,3 +1,38 @@
-############## SECTION 12  MILESTONE PROJECT 2 - BLACKJACK GAME ########################################################
+########################## SECTION 12  MILESTONE PROJECT 2 - BLACKJACK GAME ############################################
 ##### Model of a Standard Deck of Cards
 
+import random
+class Deck():
+    ###Class object attributes
+    suits = ('HEARTS', 'CLUBS', 'DIAMONDS', 'SPADES')
+    deckDic = {"ONE": 1 , "TWO": 2, "THREE": 3, "FOUR": 4, "FIVE": 5, "SIX": 6, "SEVEN": 7, "EIGHT": 8, "NINE": 9,
+                  "JACK": 10, "QUEEN": 10, "KING": 10, "ACE":(1,11)}
+
+    ###Object constructor
+    def __init__(self):
+        self.deck = []
+        for suit in Deck.suits:
+            for key in Deck.deckDic.keys():
+                self.deck.append((suit, key))
+        assert (len(self.deck) == 52)
+
+    ### Class Methods
+    def shuffleDeck(self):
+        self.deck = random.sample(self.deck, len(self.deck))
+
+    def dealCards(self, initial_deal=False, hit=False): ###Deals cards as required by player and computer at the beginning of the game
+        if initial_deal:
+            cards = random.choices(self.deck,k=2)
+            print(cards)
+        elif hit:
+            cards = [random.choice(self.deck)]
+
+        Deck.discardCards(self,cards)
+        return cards
+
+    def discardCards(self,cards): ###Discards the card (or cards) whenever an initial dealing or a hit occur.
+        if len(cards)>=2:
+            for card in cards:
+                self.deck.remove(card)
+        else:
+            self.deck.remove(cards[0])
---
+++ Complete_Python_Bootcamp/MP2_BlackJackGame/MainGame.py
@@ -0,0 +1,2 @@
+############## SECTION 12  MILESTONE PROJECT 2 - BLACKJACK GAME ########################################################
+#### Game-Logic
